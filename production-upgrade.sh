d9f532ea3b603ea672b562c161378c7d4524dd7a
brandon <brandon@brandonmcfadden.com>
2023-06-19 23:39:42 -0500

    More Autonomous Upgrades
    micro script to auto-upgrade prod

--- production-upgrade.sh
+++ production-upgrade.sh
@@ -1,8 +1,7 @@
 git stash
 git stash drop
 git pull
-pip-upgrade /home/brandon_brandonmcfadden_com/cta-reliability/requirements.txt
-sudo chmod +x production-upgrade.sh export_7_days_json_data.py export_7_days_arrivals.py export_1_month_arrivals.py main.py metra.py station_headways.py station-tracking.py
+pip-upgrade --skip-virtualenv-check /home/brandon_brandonmcfadden_com/cta-reliability/requirements.txt
 sudo systemctl restart api-service.service
 sudo systemctl restart cta-reliability.service
 sudo systemctl restart headways.service
