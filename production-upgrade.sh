827b6ab05b0b5fe016f8c1614c13f70c00bc7d7c
brandon <brandon@brandonmcfadden.com>
2023-06-19 23:27:10 -0500

    More Autonomous Upgrades
    micro script to auto-upgrade prod

--- production-upgrade.sh
+++ production-upgrade.sh
@@ -1,3 +1,5 @@
+git stash
+git stash drop
 git pull
 pip install -r requirements.txt
 chmod +x production-upgrade.sh export_7_days_json_data.py export_7_days_arrivals.py export_1_month_arrivals.py main.py metra.py station_headways.py station-tracking.py
