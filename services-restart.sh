2e27de3862c6c92e6155739b6e28215f628bddec
brandon <brandon@brandonmcfadden.com>
2023-06-19 23:20:49 -0500

    Create services-restart.sh

---
+++ services-restart.sh
@@ -0,0 +1,5 @@
+git pull
+sudo systemctl restart api-service.service
+sudo systemctl restart cta-reliability.service
+sudo systemctl restart headways.service
+sudo systemctl restart metra-reliability.service
