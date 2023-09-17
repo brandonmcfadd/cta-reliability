19af2468c32371388efac34ab14a081097890fea
brandon <brandon@brandonmcfadden.com>
2023-09-16 22:29:33 -0400

    Update production-upgrade.sh

--- production-upgrade.sh
+++ production-upgrade.sh
@@ -2,6 +2,7 @@ sudo git stash
 sudo git stash drop
 sudo git pull
 sudo chown -R brandon_brandonmcfadden_com:brandon_brandonmcfadden_com .
+sudo chmod +x production-upgrade.sh
 pip install -r /home/brandon_brandonmcfadden_com/cta-reliability/requirements.txt
 sudo systemctl restart api-service.service
 sudo systemctl restart cta-reliability.service
