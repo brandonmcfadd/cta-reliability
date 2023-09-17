895e16bd20914198f073eb23309b9319862ecede
brandon <brandon@brandonmcfadden.com>
2023-09-16 22:29:14 -0400

    Update is_the_cta_okay.py

--- is_the_cta_okay.py
+++ is_the_cta_okay.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 import os
 import json
 import tweepy
@@ -23,12 +22,6 @@ file = open(file=main_file_path + '/cta-reliability/settings.json',
             encoding='utf-8')
 settings = json.load(file)
 
-# Scheduled Runs
-weekday_runs = settings["is-cta-okay"]["weekday"]
-weekend_runs = settings["is-cta-okay"]["weekend"]
-weekday_runs_reversed = weekday_runs[::-1]
-weekend_runs_reversed = weekend_runs[::-1]
-
 
 def get_run_data_from_api():
     todays_stats_api_url = "http://rta-api.brandonmcfadden.com/api/v2/cta/get_daily_results/today"
@@ -45,16 +38,8 @@ def get_run_data_from_api():
 
 def day_of_performance_stats(data):
     system_actual = int(data["system"]["ActualRuns"])
-    last_update_api = datetime.strptime(
-        data["LastUpdated"], "%Y-%m-%dT%H:%M:%S%z")
-    last_update_int = int(datetime.strftime(last_update_api, "%H"))
-    current_datetime = datetime.now()
-    day_of_week = current_datetime.weekday()
-    if day_of_week <=5:
-        estimated_sched_runs = weekday_runs[last_update_int - 1] * 0.9
-    else:
-        estimated_sched_runs = weekend_runs[last_update_int - 1] * 0.9
-    if system_actual >= estimated_sched_runs:
+    system_scheduled = int(data["system"]["ScheduledRuns"])
+    if system_actual/system_scheduled >= 0.9:
         good_day = True
     else:
         good_day = False
@@ -70,10 +55,10 @@ def prepare_tweet_text_1(data, is_good_day_flag):
     if consistent_arrivals > 0:
         consistent_arrivals_perc = int((consistent_arrivals/system_actual)*100)
     if is_good_day_flag is True:
-        type_of_day = "CTA Rail is having a good day! To do this the CTA cut 18% of scheduled service. Check out ctaction.org/service-cuts for more."
+        type_of_day = "CTA Rail is having a good day! To do this the CTA cut 21% of scheduled service. Check out ctaction.org/service-cuts for more."
         expression = "!"
     else:
-        type_of_day = "CTA Rail is not having a good day even after cutting 18% of scheduled service. Check out ctaction.org/service-cuts for more."
+        type_of_day = "CTA Rail is not having a good day even after cutting 21% of scheduled service. Check out ctaction.org/service-cuts for more."
         expression = "."
     text_output_part_1 = f"{type_of_day}\n{system_perc}% of scheduled trains have run today{expression} Of those, {consistent_arrivals_perc}% arrived at consistent intervals.\nTo explore historical data, visit brandonmcfadden.com/cta-reliability."
     return text_output_part_1
@@ -82,6 +67,7 @@ def prepare_tweet_text_1(data, is_good_day_flag):
 def prepare_tweet_text_2(data):
     system_actual = data["system"]["ActualRuns"]
     system_sched = data["system"]["ScheduledRuns"]
+    scheduled_runs_remaining = data["system"]["ScheduledRunsRemaining"]
     system_perc = int(float(data["system"]["PercentRun"]) * 100)
     consistent_arrivals = 0
     text_output_part_2 = f"Current System Stats (actual/scheduled):\nSystem: {system_perc}% - {system_actual:,}/{system_sched:,}"
@@ -92,17 +78,10 @@ def prepare_tweet_text_2(data):
         percent_run = int(float(data["routes"][line]["PercentRun"]) * 100)
         text_output_part_2 = text_output_part_2 + \
             f"\n{line}: {percent_run}% - {actual_runs:,}/{scheduled_runs:,}"
-    last_update_api = datetime.strptime(
-        data["LastUpdated"], "%Y-%m-%dT%H:%M:%S%z")
-    last_update_int = int(datetime.strftime(last_update_api, "%H"))
-    current_datetime = datetime.now()
-    day_of_week = current_datetime.weekday()
-    if day_of_week <=5:
-        estimated_remaining = weekday_runs_reversed[last_update_int - 1]
-    else:
-        estimated_remaining = weekend_runs_reversed[last_update_int - 1]
-    text_output_part_2 = text_output_part_2 + f"\nScheduled Runs Remaining: {estimated_remaining:,}"
+    text_output_part_2 = text_output_part_2 + \
+        f"\nScheduled Runs Remaining: {scheduled_runs_remaining:,}"
     return text_output_part_2
+
 
 current_data = get_run_data_from_api()
 is_good_day = day_of_performance_stats(current_data)
@@ -119,4 +98,5 @@ status1 = api.create_tweet(text=tweet_text_1, )
 first_tweet = status1.data["id"]
 status2 = api.create_tweet(text=tweet_text_2, in_reply_to_tweet_id=first_tweet)
 second_tweet = status2.data["id"]
-print(f"sent new tweets https://twitter.com/isCTAokay/status/{first_tweet} and https://twitter.com/isCTAokay/status/{second_tweet}")
+print(
+    f"sent new tweets https://twitter.com/isCTAokay/status/{first_tweet} and https://twitter.com/isCTAokay/status/{second_tweet}")
