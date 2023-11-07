34cb3eac8ca5e6bc020f7922de2e34b9b2467dd0
brandon <brandon@brandonmcfadden.com>
2023-11-07 16:03:41 -0500

    Update production-upgrade.sh

--- production-upgrade.sh
+++ production-upgrade.sh
@@ -2,9 +2,8 @@
 python3 -m pip install --upgrade pip
 cd /home/brandon_brandonmcfadden_com/cta-reliability/
 sudo git stash
-sudo git stash -- /home/brandon_brandonmcfadden_com/cta-reliability/
-sudo git stash drop -- /home/brandon_brandonmcfadden_com/cta-reliability/
-sudo git pull -- /home/brandon_brandonmcfadden_com/cta-reliability/
+sudo git stash drop
+sudo git pull
 sudo chown -R brandon_brandonmcfadden_com:brandon_brandonmcfadden_com .
 sudo chmod +x production-upgrade.sh
 pip install -r /home/brandon_brandonmcfadden_com/cta-reliability/requirements.txt
