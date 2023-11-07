7383b10d3d8a9dda12cbd274193adc88e4bbbe58
brandon <brandon@brandonmcfadden.com>
2023-11-07 16:13:04 -0500

    Update production-upgrade.sh

--- production-upgrade.sh
+++ production-upgrade.sh
@@ -7,7 +7,7 @@ sudo git pull
 sudo chown -R brandon_brandonmcfadden_com:brandon_brandonmcfadden_com .
 sudo chmod +x production-upgrade.sh
 pip install -r /home/brandon_brandonmcfadden_com/cta-reliability/requirements.txt
-sudo systemctl restart api-service.service
 sudo systemctl restart cta-reliability.service
 sudo systemctl restart headways.service
 sudo systemctl restart metra-reliability.service
+sudo systemctl restart api-service.service
