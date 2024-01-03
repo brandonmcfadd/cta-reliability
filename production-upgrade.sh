bff250fc9bdba0a9947708c472da1958aa7c2b16
brandon <brandon@brandonmcfadden.com>
2024-01-03 00:40:20 -0600

    Update production-upgrade.sh

--- production-upgrade.sh
+++ production-upgrade.sh
@@ -4,9 +4,9 @@ python3 -m pip install --upgrade pip
 sudo git stash
 sudo git stash drop
 sudo git pull
-sudo chown -R brandon_brandonmcfadden_com:brandon_brandonmcfadden_com .
+sudo chown -R brandonmcfadden:brandonmcfadden .
 sudo chmod +x production-upgrade.sh
-pip install -r /home/brandon_brandonmcfadden_com/cta-reliability/requirements.txt
+pip install -r /home/brandonmcfadden/cta-reliability/requirements.txt
 sudo systemctl restart cta-reliability.service
 sudo systemctl restart headways.service
 sudo systemctl restart metra-reliability.service
