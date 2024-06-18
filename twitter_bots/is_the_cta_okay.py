653b76b8a0c8e58fda32f919bf04c76a07020143
brandon <brandon@brandonmcfadden.com>
2024-06-18 15:03:24 -0500

    threads api support!

--- twitter_bots/is_the_cta_okay.py
+++ twitter_bots/is_the_cta_okay.py
@@ -16,6 +16,7 @@ twitter_api_key_secret = os.getenv('IS_CTA_OKAY_API_KEY_SECRET')
 twitter_access_token = os.getenv('IS_CTA_OKAY_ACCESS_TOKEN')
 twitter_access_token_secret = os.getenv('IS_CTA_OKAY_ACCESS_TOKEN_SECRET')
 twitter_bearer_key = os.getenv('IS_CTA_OKAY_BEARER_TOKEN')
+threads_access_token = os.getenv('THREADS_ACCESS_TOKEN')
 my_api_key = os.getenv('MY_API_KEY')
 main_file_path = os.getenv('FILE_PATH')
 
@@ -57,6 +58,37 @@ def get_run_data_from_api(type):
 
     return api_response.json()
 
+def refresh_threads_access_token(token):
+    """extends threads token to last 60 more days"""
+    threads_access_token_refresh_url = f"https://graph.threads.net/refresh_access_token?grant_type=th_refresh_token&access_token={token}"
+    api_response = requests.request(
+        "GET", threads_access_token_refresh_url, timeout=30)
+    response_json = api_response.json()
+    return response_json["expires_in"]
+
+
+def create_threads_posts(text, access_token, reply_to_id=None):
+    """builds the threads post and publishes it"""
+    if reply_to_id is None:
+        threads_access_create_post = f"https://graph.threads.net/v1.0/25882170881398162/threads?media_type=TEXT&access_token={access_token}"
+    else:
+        threads_access_create_post = f"https://graph.threads.net/v1.0/25882170881398162/threads?media_type=TEXT&reply_to_id={reply_to_id}&access_token={access_token}"
+    payload = json.dumps({
+        "text": text
+        })
+    headers = {
+        'Content-Type': 'application/json'
+        }
+    api_response_1 = requests.request("POST", threads_access_create_post, headers=headers, data=payload, timeout=30)
+    response_json_1 = api_response_1.json()
+    post_id_1 = response_json_1["id"]
+    threads_access_publish_post = f"https://graph.threads.net/v1.0/25882170881398162/threads_publish?creation_id={post_id_1}&access_token={access_token}"
+    api_response_2 = requests.request(
+        "POST", threads_access_publish_post, timeout=30)
+    response_json_2 = api_response_2.json()
+    post_id_2 = response_json_2["id"]
+    return post_id_2
+    
 
 def day_of_performance_stats(data):
     """determines the type of day we having"""
@@ -191,6 +223,96 @@ def prepare_tweet_text_3(data):
     text_output_part_3 = f"On-Time Performance {text_insert} {tweet_date}{tweet_date_ending}{tweet_hour} (# on-time/actual):\nSystem: {system_perc}% • {on_time_arrivals:,}/{system_actual:,}{text_output_part_3}"
     return text_output_part_3
 
+
+def prepare_threads_text_1(data, is_good_day_flag):
+    """preps the tweet text for the first tweet"""
+    if get_date("hour") in ("0", "00"):
+        text_insert = "had"
+        tweet_date = get_date("tweet-date-yesterday")
+        tweet_date_ending = get_ordinal_suffix(int(get_date("tweet-date-yesterday-int")))
+        tweet_hour = ""
+        additional_text = check_stale_state(get_date("yesterday-short"))
+    else:
+        text_insert = "is having"
+        tweet_date = get_date("tweet-date-today")
+        tweet_date_ending = get_ordinal_suffix(int(get_date("tweet-date-today-int")))
+        tweet_hour = f" at {get_date('tweet-hour')}"
+        additional_text = check_stale_state(get_date("short"))
+    system_actual = data["system"]["ActualRuns"]
+    system_perc = int(float(data["system"]["PercentRun"]) * 100)
+    system_perc_reduced = int(
+        float(data["system"]["PrePandemicScheduledPercChange"]) * 100) * -1
+    on_time_trains = 0
+
+    for line in data["routes"]:
+        on_time_trains += int(data["routes"][line]["Trains_On_Time"])
+    if on_time_trains > 0:
+        on_time_trains_perc = int((on_time_trains/system_actual)*100)
+    if is_good_day_flag == 0:
+        type_of_day = f"🤩CTA Rail {text_insert} a great day! To do this, the CTA cut {system_perc_reduced}% of scheduled service."
+        expression = "!"
+    elif is_good_day_flag == 1:
+        type_of_day = f"😎CTA Rail {text_insert} a good day! To do this, the CTA cut {system_perc_reduced}% of scheduled service."
+        expression = "!"
+    elif is_good_day_flag == 2:
+        type_of_day = f"🤷CTA Rail {text_insert} a so-so day even with a {system_perc_reduced}% cut of scheduled service."
+        expression = "."
+    elif is_good_day_flag == 3:
+        type_of_day = f"😡CTA Rail {text_insert} a tough day even after cutting {system_perc_reduced}% of scheduled service."
+        expression = "."
+    else:
+        type_of_day = f"🤬CTA Rail {text_insert} a terrible day even after cutting {system_perc_reduced}% of scheduled service."
+        expression = "."
+    text_output_part_1 = f"{type_of_day}\n{system_perc}% of scheduled trains operated on {tweet_date}{tweet_date_ending}{tweet_hour}{expression}\n{on_time_trains_perc}% arrived at their scheduled intervals.{additional_text}\nTo explore historical data: brandonmcfadden.com/cta-reliability"
+    return text_output_part_1
+
+
+def prepare_threads_text_2(data):
+    "prepares the reply text for thread reply"
+    if get_date("hour") in ("0", "00"):
+        text_insert = "for"
+        tweet_date = get_date("tweet-date-yesterday")
+        tweet_date_ending = get_ordinal_suffix(int(get_date("tweet-date-yesterday-int")))
+        tweet_hour = ""
+    else:
+        text_insert = "As of"
+        tweet_date = get_date("tweet-date-today")
+        tweet_date_ending = get_ordinal_suffix(int(get_date("tweet-date-today-int")))
+        tweet_hour = f" at {get_date('tweet-hour')}"
+    system_actual = data["system"]["ActualRuns"]
+    system_sched = data["system"]["ScheduledRuns"]
+    scheduled_runs_remaining = data["system"]["ScheduledRunsRemaining"]
+    system_perc = int(float(data["system"]["PercentRun"]) * 100)
+
+    try:
+        scheduled_runs_remaining_text = f"{scheduled_runs_remaining:,}"
+    except: # pylint: disable=bare-except
+        scheduled_runs_remaining_text = "🤷"
+    text_output_part_1 = f"{text_insert} {tweet_date}{tweet_date_ending}{tweet_hour}\n\nService Delivery (actual/scheduled):\nSystem: {system_perc}% • {system_actual:,}/{system_sched:,}"
+    for line in data["routes"]:
+        actual_runs = data["routes"][line]["ActualRuns"]
+        scheduled_runs = data["routes"][line]["ScheduledRuns"]
+        percent_run = int(float(data["routes"][line]["PercentRun"]) * 100)
+        text_output_part_1 = text_output_part_1 + \
+            f"\n{line}: {percent_run}% • {actual_runs:,}/{scheduled_runs:,}"
+    text_output_part_1 = text_output_part_1 + \
+        f"\nRuns Remaining: {scheduled_runs_remaining_text}"
+    system_actual = data["system"]["ActualRuns"]
+    system_perc = int(float(data["system"]["PercentRun"]) * 100)
+    on_time_arrivals = 0
+    text_output_part_2 = ""
+    for line in data["routes"]:
+        on_time_arrivals += int(data["routes"][line]["Trains_On_Time"])
+        on_time_runs = data["routes"][line]["Trains_On_Time"]
+        try:
+            percent_on_time = int(float(data["routes"][line]["Trains_On_Time"]/data["routes"][line]["ActualRuns"]) * 100)
+        except: # pylint: disable=bare-except
+            percent_on_time = 0
+        text_output_part_2 = f"{text_output_part_2}\n{line}: {percent_on_time}% • {on_time_runs:,}/{data['routes'][line]['ActualRuns']:,}"
+    system_perc = int(float(on_time_arrivals/system_actual) * 100)
+    text_output_part_3 = f"{text_output_part_1}\n\nOn-Time Performance (# on-time/actual):\nSystem: {system_perc}% • {on_time_arrivals:,}/{system_actual:,}{text_output_part_2}"
+    return text_output_part_3
+
 if get_date("hour") in ("0", "00"):
     current_data = get_run_data_from_api("yesterday")
 else:
@@ -200,13 +322,9 @@ is_good_day = day_of_performance_stats(current_data)
 tweet_text_1 = prepare_tweet_text_1(current_data, is_good_day)
 tweet_text_2 = prepare_tweet_text_2(current_data)
 tweet_text_3 = prepare_tweet_text_3(current_data)
-
-print(tweet_text_1)
-print()
-print(tweet_text_2)
-print()
-print(tweet_text_3)
-print()
+threads_text_1 = prepare_threads_text_1(current_data, is_good_day)
+threads_text_2 = prepare_threads_text_2(current_data)
+print("Threads Token Refresh Successful. Expires in", refresh_threads_access_token(threads_access_token), "sec")
 
 api = tweepy.Client(twitter_bearer_key, twitter_api_key, twitter_api_key_secret,
                     twitter_access_token, twitter_access_token_secret)
@@ -220,3 +338,7 @@ status3 = api.create_tweet(text=tweet_text_3, in_reply_to_tweet_id=second_tweet)
 third_tweet = status3.data["id"]
 print(
     f"sent new tweets https://twitter.com/isCTAokay/status/{first_tweet} and https://twitter.com/isCTAokay/status/{second_tweet} and https://twitter.com/isCTAokay/status/{third_tweet}")
+
+threads_post_1 = create_threads_posts(threads_text_1, threads_access_token)
+threads_post_2 = create_threads_posts(threads_text_2, threads_access_token, threads_post_1)
+print(f"Sent new threads posts with IDs: {threads_post_1} and {threads_post_2}")
