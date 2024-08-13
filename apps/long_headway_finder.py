ab8b811bdc1c23b1cde4e68bd97cb685cb26729e
brandon <brandon@brandonmcfadden.com>
2024-08-13 18:56:37 -0400

    lets use the train track API instead

--- apps/long_headway_finder.py
+++ apps/long_headway_finder.py
@@ -1,16 +1,22 @@
 """This checks for the longest Headway Gap in the CTA System Map"""
 import os
 import json
-import re
 from datetime import datetime, timedelta
 import requests  # Used for API Calls
 from dotenv import load_dotenv  # Used to Load Env Var
+import urllib3
+urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+try:
+    requests.packages.urllib3.contrib.pyopenssl.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+except AttributeError:
+    pass  # no pyopenssl support used / needed / available
 
 # Load .env variables
 load_dotenv()
 
 # ENV Variables
 main_file_path = os.getenv('FILE_PATH')
+api_key = os.getenv('TRAIN_HEADWAY_API_KEY')
 
 # Settings
 file = open(file=main_file_path + 'settings.json',
@@ -18,10 +24,12 @@ file = open(file=main_file_path + 'settings.json',
             encoding='utf-8')
 settings = json.load(file)
 
+tt_api_url = settings["train-tracker"]["api-url"]
 train_tracker_url_map = settings["train-tracker"]["special-map-url"]
+station_ids = settings["train-tracker"]["all-station-ids"]
 metra_tracker_url = settings["metra-api"]["trips-api-url"]
 bus_tracker_url = settings["bus-tracker-api"]["api-url"]
-line_names = {"Red":"Red","Pur":"Purple","Yel":"Yellow","Blu":"Blue","Pnk":"Pink","Grn":"Green","Brn":"Brown","Org":"Orange"}
+line_names = {"Red":"Red","P":"Purple","Y":"Yellow","Blue":"Blue","Pink":"Pink","G":"Green","Brn":"Brown","Org":"Orange"}
 
 def get_date(date_type):
     """formatted date shortcut"""
@@ -38,61 +46,62 @@ def get_date(date_type):
     return date
 
 
-def train_api_call_to_cta_map():
-    """Gotta talk to the CTA and get Train Times!"""
-    print("Making CTA Train Map API Call...")
-    try:
-        api_response = requests.get(train_tracker_url_map, timeout=10)
-        api_output = api_response.json()
-        api_response.raise_for_status()
-    except requests.exceptions.HTTPError as errh:
-        print("Map - Http Error:", errh)
-    except requests.exceptions.ConnectionError as errc:
-        print("Map - Error Connecting:", errc)
-    except requests.exceptions.Timeout as errt:
-        print("Map - Timeout Error:", errt)
-    except requests.exceptions.RequestException as err:
-        print("Map - Error in API Call to Train Tracker", err)
-    return api_output
+def calc_tt_eta(date_1, date_2):
+    """Takes the difference between two times and returns the minutes"""
+    date_1 = datetime.strptime(date_1, "%Y-%m-%dT%H:%M:%S")
+    date_2 = datetime.strptime(date_2, "%Y-%m-%dT%H:%M:%S")
+    difference = date_2 - date_1
+    difference_in_minutes = int(difference / timedelta(minutes=1))
+    return difference_in_minutes
 
-def find_headways(response):
+
+def call_to_cta_api(station_id_input, url):
+    """Gotta talk to the CTA and get Train Times"""
+    print(f"Making CTA Train API Call for stop: {station_id_input}")
+    try:
+        api_response = requests.get(
+            url.format(api_key, station_id_input), timeout=30)
+    except: # pylint: disable=bare-except
+        print("Error in API Call to Train Tracker")
+    return api_response.json()
+
+def find_headways(response, start_time):
     """takes output from the API and pulls out all the stations and headways"""
-    headways = {"LastUpdated":get_date("full"),"Lines":{}}
-    for line in response['dataObject']:
-        for marker in line["Markers"]:
-            for prediction in marker["Predictions"]:
-                if str(prediction[2]) == "<b>Due</b>":
-                    eta = 0
-                elif str(prediction[2]) == "Delayed":
-                    eta = None
-                else:
-                    minutes = re.sub('[^0-9]+', '', str(prediction[2]))
-                    eta = int(minutes)
-                if eta is not None:
-                    line_name = line_names[marker["LineName"]]
-                    station_name = str(prediction[1])
-                    station_id = int(prediction[0])
-                    if line_name not in headways["Lines"]:
-                        headways["Lines"] = {**headways["Lines"],**{line_name: {}}}
-                    if station_name not in headways["Lines"][line_name]:
-                        headways["Lines"][line_name] = {**headways["Lines"][line_name],**{station_name: {}}}
-                    if str(marker["DestName"]).startswith(("O'Hare", "Midway")):
-                        if str(marker["DestName"]).startswith("O'Hare"):
-                            destination = "O'Hare"
-                        else:
-                            destination = "Midway"
-                    else:
-                        destination = marker["DestName"]
-                    if destination in ("UIC-Halsted", "Cottage Grove"):
-                        style = f"cta-{line_name}-line-inverted"
-                    else:
-                        style = f"cta-{line_name}-line"
-                    if destination not in headways["Lines"][line_name][station_name]:
-                        headways["Lines"][line_name][station_name] = {**headways["Lines"][line_name][station_name],**{destination: {"Arrivals":[],"Headways":[],"Trains":{}}}}
-                    else:
-                        headways["Lines"][line_name][station_name][destination]["Arrivals"].append(eta)
-                        headways["Lines"][line_name][station_name][destination]["Arrivals"].sort()
-                        headways["Lines"][line_name][station_name][destination]["Trains"] = {**headways["Lines"][line_name][station_name][destination]["Trains"],**{str(eta):{"TimeToArrival":eta,"Headway":None,"Line":line_name,"RunNumber":marker["RunNumber"],"Destination":destination, "StationName":station_name, "StationID":station_id, "Style": style}}}
+    headways = {"LastUpdated":start_time,"Lines":{}}
+    for train in response['ctatt']["eta"]:
+        prediction = train["prdt"]
+        arrival = train["arrT"]
+        estimated_time = int(calc_tt_eta(prediction, arrival))
+        line_name = line_names[train["rt"]]
+        station_name = train["staNm"]
+        current_station_id = train["staId"]
+        destination = train["destNm"]
+        if train["isSch"] == "1":
+            scheduled = True
+        else:
+            scheduled = False
+        if line_name not in headways["Lines"]:
+            headways["Lines"] = {**headways["Lines"],**{line_name: {}}}
+        if station_name not in headways["Lines"][line_name]:
+            headways["Lines"][line_name] = {**headways["Lines"][line_name],**{station_name: {}}}
+        if destination.startswith(("O'Hare", "Midway")):
+            if destination.startswith("O'Hare"):
+                destination = "O'Hare"
+            else:
+                destination = "Midway"
+        if destination in ("UIC-Halsted", "Cottage Grove"):
+            style = f"cta-{line_name}-line-inverted"
+        else:
+            style = f"cta-{line_name}-line"
+        if destination not in headways["Lines"][line_name][station_name]:
+            headways["Lines"][line_name][station_name] = {**headways["Lines"][line_name][station_name],**{destination: {"Arrivals":[],"Headways":[],"Trains":{}}}}
+            headways["Lines"][line_name][station_name][destination]["Arrivals"].append(estimated_time)
+            headways["Lines"][line_name][station_name][destination]["Arrivals"].sort()
+            headways["Lines"][line_name][station_name][destination]["Trains"] = {**headways["Lines"][line_name][station_name][destination]["Trains"],**{str(estimated_time):{"TimeToArrival":int(estimated_time),"Headway":None,"Line":line_name,"RunNumber":train["rn"],"Destination":destination, "StationName":station_name, "StationID":current_station_id, "IsScheduled":scheduled, "Style": style}}}
+        else:
+            headways["Lines"][line_name][station_name][destination]["Arrivals"].append(estimated_time)
+            headways["Lines"][line_name][station_name][destination]["Arrivals"].sort()
+            headways["Lines"][line_name][station_name][destination]["Trains"] = {**headways["Lines"][line_name][station_name][destination]["Trains"],**{str(estimated_time):{"TimeToArrival":int(estimated_time),"Headway":None,"Line":line_name,"RunNumber":train["rn"],"Destination":destination, "StationName":station_name, "StationID":current_station_id, "IsScheduled":scheduled, "Style": style}}}
     return headways
 
 
@@ -123,11 +132,15 @@ def find_the_big_ones(headways):
                     if train["Headway"] > biggest_gap["Headway"]:
                         biggest_gap = train
     headways = {**headways,**{"BiggestGap":biggest_gap}}
-    return(headways)
+    return headways
 
+current_time = get_date("full")
+for station_id in station_ids:
+    api_response_value = call_to_cta_api(station_id, tt_api_url)
+    if "eta" in api_response_value["ctatt"]:
+        headways_input = find_headways(api_response_value, current_time)
+calculated_headways = calculate_headways(headways_input)
+headways_output = find_the_big_ones(calculated_headways)
 
 with open(main_file_path + "train_arrivals/special/long_headways.json", 'w', encoding="utf-8") as fp2:
-    headways_input = find_headways(train_api_call_to_cta_map())
-    calculated_headways = calculate_headways(headways_input)
-    headways_output = find_the_big_ones(calculated_headways)
     json.dump(headways_output, fp2, indent=4,  separators=(',', ': '))
