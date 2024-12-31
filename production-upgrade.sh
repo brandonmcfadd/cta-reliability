2c3ed1d6703ca2dcfd185adf8c3fdd4ef314178c
brandon <brandon@brandonmcfadden.com>
2024-12-31 16:45:42 -0600

    Update production-upgrade.sh

--- production-upgrade.sh
+++ production-upgrade.sh
@@ -11,3 +11,4 @@ sudo systemctl restart cta-reliability.service
 sudo systemctl restart headways.service
 sudo systemctl restart metra-reliability.service
 sudo systemctl restart api-service.service
+sudo systemctl restart metra-alerts.service
